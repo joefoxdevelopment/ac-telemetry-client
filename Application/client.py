65978f7fb1e50f4c9c1941d815f048e6068f2287
Joe Fox <joefox@Joes-MBP.lan>
2019-10-27 19:19:45 +0000

    Add validation for host and port number in Application/Client constructor

--- Application/client.py
+++ Application/client.py
@@ -1,11 +1,16 @@
+import re
+
 class Client:
 
     host = None
     port = None
 
     def __init__(self, host, port):
+        if [] == re.findall('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', host):
+            raise Exception('Host is not a valid IPv4 address. Received: ' + host)
+
+        if [] == re.findall('^\d{1,5}$', port):
+            raise Exception('Host is not a valid port number. Received: ' + port)
+
         self.host = host
         self.port = port
-
-        print(self.host)
-        print(self.port)
